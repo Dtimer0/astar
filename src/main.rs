c114f228a82c26477e70d8e650aa5bfd2bc9009e
Jude Packard-Jones <judepackardjones@gmail.com>
2023-11-16 09:39:36 -0500

    + start node create, 2d array creation

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 fn main() {
     let (x_length, y_length) = (5, 5);
-    // Start of a*
+    let mut map: Vec<Vec<NodeType>> = create_2d_array(x_length, y_length);
+    map.set_start(4, 0).unwrap();
+    map.iter().for_each(|it| {println!(); it.iter().for_each(|x| {print!("{:#?}, ", x)})});
 }
+trait X {
+    fn set_start(&mut self, x: usize, y: usize) -> Result<(), &'static str>;
+    fn set_end(&mut self, x: usize, y: usize) -> Result<(), &'static str>;
+    fn set_blocks(&mut self, coordinates: Vec<(usize, usize)>) ->Result<(), &'static str>;
+}
+#[derive(Debug, PartialEq, Clone)]
 pub enum NodeType {
     Path,
     Block,
     Start, 
     End
 }
-pub enum NodeState {
-    Occupied,
-    Unoccupied,
+pub fn create_2d_array(x_length: i64, y_length: i64) -> Vec<Vec<NodeType>> {
+    vec![vec![NodeType::Path; x_length as usize]; y_length as usize]
 }
-pub fn create_2D_array(x_length: i64, y_legnth: i64) -> Vec<Vec<(i64, i64, NodeType, NodeState)>> {
-    todo!();
-}
-pub fn setStart(coordinate: (i64, i64)) -> Result<Vec<Vec<(i64, i64, NodeType, NodeState)>>, &'static str> {
-    todo!();
-}
-pub fn setEnd(coordinate: (i64, i64)) -> Result<Vec<Vec<(i64, i64, NodeType, NodeState)>>, &'static str> {
-    todo!();
-}
-pub fn setBlocks(coordinate: Vec<(i64, i64)>) -> Result<Vec<Vec<(i64, i64, NodeType, NodeState)>>, &'static str> {
-    todo!();
+impl X for Vec<Vec<NodeType>> {
+    fn set_start(&mut self, x: usize, y: usize) -> Result<(), &'static str> {
+        if !in_range(x, y, self.len(), self[0].len()) {
+            return Err("Out of range");
+        }
+        self[x][y] = NodeType::Start;
+        Ok(())
+    }
+    fn set_end(&mut self, x: usize, y: usize) -> Result<(), &'static str> {
+        todo!();
+    }
+    fn set_blocks(&mut self, coordinates: Vec<(usize, usize)>) ->Result<(), &'static str> {
+        todo!();
+    }
+}   
+
+pub fn in_range(x: usize, y: usize, x_length: usize, y_length: usize) -> bool {
+    x < x_length && y < y_length 
 }
