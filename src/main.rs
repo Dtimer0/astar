e3cfa12804dea85c8a5443dc247985ec4d729e0a
Jude Packard-Jones <judepackardjones@gmail.com>
2023-11-20 15:06:03 -0500

    + algorithm parts

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,82 @@
+use std::ops::Index;
+
 fn main() {
     let start = (4, 0);
     let end = (0, 0);
-    let mut open_set : Vec<(usize, usize)> = Vec::new();
-    let mut closed_set : Vec<(usize, usize)> = Vec::new();
+    let mut current = (0, 0);
+    let mut frontier_set : Vec<(usize, usize)> = Vec::new();
+    let mut unexplored_set : Vec<(usize, usize)> = Vec::new();
+    let mut explored_set : Vec<(usize, usize)> = Vec::new();
     let (x_length, y_length) = (5, 5);
     let mut map: Vec<Vec<NodeType>> = create_2d_array(x_length, y_length);
     map.set_start(start).unwrap();
     map.set_end(end).unwrap();
     map.set_blocks(Vec::from([(1, 0), (1, 1), (1, 3), (1, 4)])).unwrap();
     map.iter().for_each(|it| {println!(); it.iter().for_each(|x| {print!("[{:#?}] ", x)})});
-    println!("{}", get_g_cost((4, 1), (2, 0)));
-    for neighbour in get_neighbours(map, (0, 0)) {
-        println!("{:?}, {}, {}", neighbour, get_f_cost(neighbour, start, end), get_h_cost(neighbour, end));
+    for i in 0..x_length {
+        for j in 0..y_length {
+            if map[i as usize][j as usize] == NodeType::Start {
+                current = (i as usize, j as usize);
+            }
+            else if map[i as usize][j as usize] == NodeType::Path {
+                unexplored_set.push((i as usize, j as usize));
+            }
+        }
     }
+    println!();
+    let mut current_lowest_f_cost = -1;
+    let mut lowest_f_costs: Vec<(i64, i64)> = Vec::new();
+    let mut neighbour_list: Vec<(i64, i64)> = Vec::new();
+    // Start of algorithm
+    frontier_set.push(current);
+    loop {
+        lowest_f_costs.clear();
+        for neighbours in frontier_set.clone() {
+            neighbour_list.append(&mut get_neighbours(map.clone(), (neighbours.0 as i64, neighbours.1 as i64)));
+        }
+        for neighbour in neighbour_list.clone() {
+            if current_lowest_f_cost == -1 {
+                current_lowest_f_cost = get_f_cost(neighbour, start, end);
+                lowest_f_costs.push(neighbour);
+            }
+            else if get_f_cost(neighbour, start, end) < current_lowest_f_cost {
+                current_lowest_f_cost = get_f_cost(neighbour, start, end);
+                lowest_f_costs.clear();
+                lowest_f_costs.push(neighbour);
+            }
+            else if get_f_cost(neighbour, start, end) == current_lowest_f_cost {
+                lowest_f_costs.push(neighbour);
+            }
+            println!("{:?}, F_cost: {}, H_cost: {}", neighbour, get_f_cost(neighbour, start, end), get_h_cost(neighbour, end));
+            frontier_set.push((neighbour.0 as usize, neighbour.1 as usize));
+        }
+        let mut current_lowest_h_cost = -1;
+        lowest_f_costs.iter().for_each(|x| if explored_set.contains(&(x.clone().0 as usize, x.clone().1 as usize)) {lowest_f_costs.remove(lowest_f_costs.iter().position(|&y| y == *x).unwrap());});
+        for lowest_f_cost in lowest_f_costs.clone() {
+            if lowest_f_cost == end {
+                println!("Found end");
+                break;
+            }
+            else {
+                if current_lowest_h_cost == -1 {
+                    current_lowest_h_cost = get_h_cost(lowest_f_cost, end);
+                    current = (lowest_f_cost.0 as usize, lowest_f_cost.1 as usize);
+                }
+                else if get_h_cost(lowest_f_cost, end) < current_lowest_h_cost {
+                    current_lowest_h_cost = get_h_cost(lowest_f_cost, end);
+                    current = (lowest_f_cost.0 as usize, lowest_f_cost.1 as usize);
+                }
+            }
+        }
+        let index = unexplored_set.iter().position(|&x| x == current).unwrap();
+        unexplored_set.remove(index);
+        let index = frontier_set.iter().position(|&x| x == current).unwrap();
+        frontier_set.remove(index);
+        explored_set.push(current);
+        println!("{}, {:?}", current_lowest_f_cost, lowest_f_costs);
+    }   
 }
+
 trait X {
     fn set_start(&mut self, start: (i64, i64)) -> Result<(), &'static str>;
     fn set_end(&mut self, end: (i64, i64)) -> Result<(), &'static str>;
